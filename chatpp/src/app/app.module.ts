ad0d4489485bce41f6b50591a782d9da824baa26
admin <admin@LP-GD9NKC2.HCLT.CORP.HCL.IN>
2018-09-08 13:01:15 +0530

    component created

--- chatpp/src/app/app.module.ts
+++ chatpp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { ChatDetailComponent } from './components/chat-detail/chat-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    ChatDetailComponent
   ],
   imports: [
     BrowserModule
